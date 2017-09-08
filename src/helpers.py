f3f64edc4040f25bed62ab66625b0c4edc88ea1a
Christopher L <christopherlee199398@gmail.com>
2017-09-08 23:07:58 +0800

    add sort functions

--- src/helpers.py
+++ src/helpers.py
@@ -7,10 +7,248 @@
     :copyright: (c) 2017 by 0xE8551CCB.
     :license: MIT, see LICENSE for more details.
 """
+import random
+from functools import wraps
 
-from functools import wraps, lru_cache
+__all__ = ('simple_cache',)
 
 
+#############################
+# Bubble sort
+# Time complexity:
+#   - Best: O(n)
+#   - Average: O(n^2)
+#   - Worst: O(n^2)
+# Space complexity: O(1)
+# Stability: Yes
+#############################
+
+def bubble_sort(seq=None):
+    if seq is None or len(seq) <= 1:
+        return seq
+
+    length = len(seq)
+    for i in range(length):
+        is_swapped = False
+        for j in range(length - 1, i, -1):
+            if seq[j - 1] > seq[j]:
+                is_swapped = True
+                seq[j - 1], seq[j] = seq[j], seq[j - 1]
+
+        if is_swapped is False:
+            break
+
+    return seq
+
+
+#############################
+# Selection sort
+# Time complexity:
+#   - Best: O(n^2)
+#   - Average: O(n^2)
+#   - Worst: O(n^2)
+# Space complexity: O(1)
+# Stability: Yes
+#############################
+
+def selection_sort(seq=None):
+    if seq is None or len(seq) <= 1:
+        return seq
+
+    length = len(seq)
+    for i in range(0, length):
+        min_index = i
+
+        for j in range(i + 1, length):
+            if seq[j] < seq[min_index]:
+                min_index = j
+
+        if i != min_index:
+            seq[i], seq[min_index] = seq[min_index], seq[i]
+
+    return seq
+
+
+#############################
+# Insertion sort
+# Time complexity:
+#   - Best: O(n)
+#   - Average: O(n^2)
+#   - Worst: O(n^2)
+# Space complexity: O(1)
+# Stability: Yes
+#############################
+
+def insertion_sort(seq=None):
+    if seq is None or len(seq) <= 1:
+        return seq
+
+    length = len(seq)
+
+    for i in range(1, length):
+        val = seq[i]
+
+        j = i
+        while j > 0 and seq[j - 1] > val:
+            seq[j] = seq[j - 1]
+            j -= 1
+
+        seq[j] = val
+
+    return seq
+
+
+#############################
+# Shell sort
+# Time complexity:
+#   - Best: O(n log^2 n)
+#   - Worst: O(n^2)
+# Space complexity: O(1)
+# Stability: No
+#############################
+
+def shell_sort(seq=None):
+    if seq is None or len(seq) <= 1:
+        return seq
+
+    length = len(seq)
+    gap = length // 2
+    while gap > 0:
+        for i in range(gap, length):
+            val = seq[i]
+            j = i
+            while j >= gap and seq[j - gap] > val:
+                seq[j] = seq[j - gap]
+                j -= gap
+
+            seq[j] = val
+
+        gap //= 2
+
+    return seq
+
+
+#############################
+# Merge sort
+# Time complexity:
+#   - Best: O(nlogn)
+#   - Average: O(nlogn)
+#   - Worst: O(nlogn)
+# Space complexity: O(n)
+# Stability: Yes
+#############################
+
+def merge_sort(seq=None):
+    if seq is None or len(seq) <= 1:
+        return seq
+
+    def merge(left_, right_):
+        from collections import deque
+        merged, left_, right_ = deque(), deque(left_), deque(right_)
+        while left_ and right_:
+            merged.append(left_.popleft() if left_[0] < right_[0] else right_.popleft())
+        merged.extend(left_ or right_)
+        return list(merged)
+
+    mid = len(seq) // 2
+    left = merge_sort(seq[:mid])
+    right = merge_sort(seq[mid:])
+    return merge(left, right)
+
+
+#############################
+# Heap sort
+# Time complexity:
+#   - Best: O(nlogn)
+#   - Average: O(nlogn)
+#   - Worst: O(nlogn)
+# Space complexity: O(1)
+# Stability: No
+#############################
+
+def heap_sort(seq=None):
+    """Heap sort
+    1. build heap
+    2. swap with the element on the right side (keep bigger element on the right)
+    3. rebuild heap(heapify)
+    """
+    if seq is None or len(seq) <= 1:
+        return seq
+
+    def heapify(seq, index, length):
+        max_index = index
+        left = index * 2 + 1
+        right = index * 2 + 2
+        if left < length and seq[left] > seq[max_index]:
+            max_index = left
+        if right < length and seq[right] > seq[max_index]:
+            max_index = right
+
+        if max_index != index:
+            seq[index], seq[max_index] = seq[max_index], seq[index]
+            heapify(seq, max_index, length)
+
+    def build_heap(seq, length):
+        for j in range(length // 2, -1, -1):
+            heapify(seq, j, length)
+
+        return seq
+
+    length_ = len(seq)
+    seq = build_heap(seq, length_)
+
+    for i in range(length_ - 1, 0, -1):
+        seq[0], seq[i] = seq[i], seq[0]
+        heapify(seq, 0, i)
+
+    return seq
+
+
+#############################
+# Quick sort
+# Time complexity:
+#   - Best: O(nlogn)
+#   - Average: O(nlogn)
+#   - Worst: O(n^2)
+# Space complexity: O(logn) ~ O(n)
+# Stability: No
+#############################
+
+def quick_sort(seq=None):
+    """Quick sort
+    1. choose a pivot
+    1. make partition (left < pivot < right)
+    """
+    if seq is None or len(seq) <= 1:
+        return seq
+
+    def partition(left, right):
+        pivot = random.randrange(left, right)
+        pivot_value = seq[pivot]
+        seq[pivot], seq[right] = seq[right], seq[pivot]
+
+        stored_index = left
+        for i in range(left, right):
+            if seq[i] < pivot_value:
+                seq[i], seq[stored_index] = seq[stored_index], seq[i]
+                stored_index += 1
+
+        seq[stored_index], seq[right] = seq[right], seq[stored_index]
+        return stored_index
+
+    def sort(left, right):
+        if left < right:
+            pivot = partition(left, right)
+            sort(left, pivot - 1)
+            sort(pivot + 1, right)
+
+    sort(0, len(seq) - 1)
+    return seq
+
+
+#############################
+# simple cache
+#############################
 def _make_key(args, kwargs):
     key = args
 
@@ -31,3 +269,13 @@ def simple_cache(func):
         return cache[key]
 
     return _
+
+
+if __name__ == '__main__':
+    print(bubble_sort([3, 1, 0, 8]))
+    print(selection_sort([3, 1, 0, 8]))
+    print(insertion_sort([3, 1, 0, 8]))
+    print(shell_sort([3, 1, 0, 8]))
+    print(merge_sort([3, 1, 0, 8]))
+    print(heap_sort([3, 1, 0, 8]))
+    print(quick_sort([3, 1, 1, 8]))
