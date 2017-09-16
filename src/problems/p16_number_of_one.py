482975713ae9d555621d4c1d5201e3343fb3e39c
Christopher L <christopherlee199398@gmail.com>
2017-09-16 18:49:42 +0800

    update p16

--- src/problems/p16_number_of_one.py
+++ src/problems/p16_number_of_one.py
@@ -8,6 +8,9 @@
     如，把 9 表示成二进制是 1001，有 2 位是 1，故返回结果
     位 2。
 
+    这里最重要的思路：把一个整数减去 1 后再和原来的整数做位与
+    运算，得到的结果相当于把整数的二进制表示中最右边的 1 变成
+    0。
     :copyright: (c) 2017 by 0xE8551CCB.
     :license: MIT, see LICENSE for more details.
 """
@@ -73,6 +76,34 @@ def number_of_one3(n):
 
     return cnt
 
+
+#
+# 拓展题
+#
+
+def is_power_of_two(n):
+    """2 的整数次方的值，其中仅有一个 1
+    故将其减去 1 后再与自身做与运算，判断
+    结果是否 0 即可。
+    """
+    return (n - 1) & n == 0
+
+
+def change_bits_to_convert_m_to_n(m, n):
+    """
+    两个整数 m 和 n，计算需要改变 m 二进制中的多少位才能得到 n。
+    """
+    # 第一步，做异或运算
+    result = m ^ n
+
+    # 统计 1 的个数
+    cnt = 0
+
+    while result:
+        cnt += 1
+        result = result & (result - 1)
+
+    return cnt
 
 if __name__ == '__main__':
     print(number_of_one2(9))
--- tests/test_p16.py
+++ tests/test_p16.py
@@ -7,7 +7,11 @@
 """
 
 import pytest
-from src.problems.p16_number_of_one import number_of_one1, number_of_one2, number_of_one3
+from src.problems.p16_number_of_one import (number_of_one1,
+                                            number_of_one2,
+                                            number_of_one3,
+                                            is_power_of_two,
+                                            change_bits_to_convert_m_to_n)
 
 
 @pytest.fixture(params=[number_of_one1, number_of_one2, number_of_one3])
@@ -20,3 +24,15 @@ def test_number_of_one(fn):
     assert fn(int('1', 2)) == 1
     assert fn(int('101010', 2)) == 3
     assert fn(int('1111', 2)) == 4
+
+
+def test_is_power_of_two():
+    assert is_power_of_two(1) is True
+    assert is_power_of_two(2) is True
+    assert is_power_of_two(3) is False
+    assert is_power_of_two(8) is True
+
+
+def test_change_bits_to_convert_m_to_n():
+    assert change_bits_to_convert_m_to_n(10, 13) == 3
+    assert change_bits_to_convert_m_to_n(int('1000', 2), int('1011', 2)) == 2
